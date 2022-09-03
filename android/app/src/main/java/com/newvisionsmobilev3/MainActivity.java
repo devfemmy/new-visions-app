5136d3b231b2a8548cc3f2f00544239e88ffc886
devfemmy <odoyoakereleisrael@gmail.com>
2022-09-03 20:59:16 +0100

    updating

--- android/app/src/main/java/com/newvisionsmobilev3/MainActivity.java
+++ android/app/src/main/java/com/newvisionsmobilev3/MainActivity.java
@@ -12,11 +12,6 @@ public class MainActivity extends ReactActivity {
    * rendering of the component.
    */
   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this);  // here
-      super.onCreate(savedInstanceState);
-  }
-  @Override
   protected String getMainComponentName() {
     return "newVisionsMobileV3";
   }
