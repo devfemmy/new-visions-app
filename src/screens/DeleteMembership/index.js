3edb16808487de80cfeebd1881fe3da5c42adc90
devfemmy <odoyoakereleisrael@gmail.com>
2022-09-07 15:39:08 +0100

    no break changes

--- src/screens/DeleteMembership/index.js
+++ src/screens/DeleteMembership/index.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import { Loader } from '../../components/Loader';
+import { AppContext } from '../../context/AppState';
+import HomePageService from '../../services/userServices';
 
 const DeleteMembership = () => {
+    const [loading, setLoading] = useState(false);
+     const {onLogout} = useContext(AppContext);
+    useEffect(() => {
+        Alert.alert(
+            "Delete Membership",
+            "Are you sure you want to delete membership?",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "DELETE", onPress: () =>  deleteUser() }
+            ]
+          ); 
+    }, [])
+
+    const deleteUser = async () => {
+        setLoading(true);
+        try {
+          const res = await HomePageService.deleteUser();
+          console.log('res', res)
+          setLoading(false);
+          onLogout();
+        return res;
+        } catch (err) {
+            // console.log('err', err)
+          setLoading(false);
+        } 
+      }
     return (
         <View style={styles.container}>
+             <Loader visible={loading} /> 
             <Text>DeleteMembership</Text>
         </View>
     )
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -21,6 +21,11 @@ export default class HomePageService {
         return res?.data || res
     }
 
+    static async deleteUser(data) {
+        const res = await baseAxios.post('/changeUserStatus', data)
+        return res?.data || res
+    }
+
     static async getSubjectDetails(data) {
         const res = await baseAxios.post('/getSubjectDetails', data)
         return res?.data || res
