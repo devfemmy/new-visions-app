661a3356776ea3689f4d56ef02142d74dd477377
devfemmy <odoyoakereleisrael@gmail.com>
2022-09-13 22:33:21 +0100

    updates

--- src/screens/FullLessonSubscription/ChooseTime.js
+++ src/screens/FullLessonSubscription/ChooseTime.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable react/prop-types */
 import React, { useContext, useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native';
+import I18n from 'i18n-js';
 import { SubContext } from '.';
 import StageCard from '../../components/StageCard'
 
@@ -26,16 +27,24 @@ const ChooseTime = ({getGroupDaysData}) => {
   })
   return(
     <View style={styles.container}>
-      {getGroupDaysData?.map((item, index) => <StageCard show
-      navigateSubjects={() => {}}
-        stage={item}
-        group
-        reducedHeight
-        activeLevel={activeLevel}
-        setActiveLevel={setActiveLevel}
-        // activeStage={activeStage}
-        setActiveStage={setActiveStage}
-        dark text={item.start && `${item.start} - ${item.end}`} />)}
+      {getGroupDaysData?.map((item, index) => {
+        const daysOfWeek = [I18n.t('Saturday'), I18n.t('Sunday'),I18n.t('Monday'),I18n.t('Tuesday'),I18n.t('Wednesday'),I18n.t('Thursday'),I18n.t('Friday'), ];
+        const specialIndex = `${item?.day_id - 1}`;
+        const dayOfWeek = daysOfWeek[specialIndex]
+        return (
+          <StageCard show
+            navigateSubjects={() => {}}
+              stage={item}
+              group
+              reducedHeight
+              activeLevel={activeLevel}
+              setActiveLevel={setActiveLevel}
+              // activeStage={activeStage}
+              setActiveStage={setActiveStage}
+              text2={dayOfWeek}
+              dark text={item.start && `${item.start} - ${item.end}`} />
+              )
+      })}
     </View>
   )
 };
